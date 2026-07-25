74e83d5d05844159315ea1169cd0aee7723ec66f
Quang Huy <huytq.developer@gmail.com>
2026-07-25 10:00:52 +0700

    refactor: merge partial ApplicationDbContext into single file under Data/

--- src/Infrastructure/Data/ApplicationDbContext.cs
+++ src/Infrastructure/Data/ApplicationDbContext.cs
@@ -3,7 +3,8 @@ using TaskItemStatus = Domain.TaskItemStatus;
 
 namespace Infrastructure;
 
-public partial class ApplicationDbContext
+public sealed class ApplicationDbContext(
+    DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
     // Audit
     public DbSet<AuditLog> AuditLogs => Set<AuditLog>();
@@ -55,6 +56,29 @@ public partial class ApplicationDbContext
     public DbSet<NotificationTemplate> NotificationTemplates => Set<NotificationTemplate>();
     public DbSet<NotificationTriggerBinding> NotificationTriggerBindings => Set<NotificationTriggerBinding>();
     public DbSet<UserNotification> UserNotifications => Set<UserNotification>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            var clrType = entityType.ClrType;
+            if (typeof(ISoftDeletable).IsAssignableFrom(clrType))
+                typeof(ApplicationDbContext)
+                    .GetMethod(nameof(SetSoftDeleteFilter), BindingFlags.NonPublic | BindingFlags.Instance)!
+                    .MakeGenericMethod(clrType)
+                    .Invoke(this, [modelBuilder]);
+        }
+    }
+
+    private void SetSoftDeleteFilter<TEntity>(ModelBuilder modelBuilder)
+        where TEntity : class, ISoftDeletable
+    {
+        modelBuilder.Entity<TEntity>()
+            .HasQueryFilter(e => !e.IsDeleted);
+    }
 
     public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
