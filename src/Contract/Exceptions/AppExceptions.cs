458c07168c28900cfc3fca3e6769a92d8962da95
Quang Huy <huytq.developer@gmail.com>
2026-07-25 10:02:12 +0700

    fix: add Contract namespace to AppExceptions.cs

--- src/Contract/Exceptions/AppExceptions.cs
+++ src/Contract/Exceptions/AppExceptions.cs
@@ -1,4 +1,4 @@
-using Contract;
+namespace Contract;
 
 public sealed class BadRequestException(string message, IDictionary<string, string[]>? errors = null)
     : AppException(message, (int)HttpStatusCode.BadRequest, errors);
